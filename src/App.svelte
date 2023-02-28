8289afe4ec9402a537f00619b3863b7807b63ef4
jakeww <wlostpiotr@gmail.com>
2023-02-28 13:44:08 +0100

    created nav style

--- src/App.svelte
+++ src/App.svelte
@@ -10,11 +10,16 @@
       <li class="nav-element"><a href=".">Contact Me</a></li>
     </ul>
   </nav>
+
+  <!-- end of navbar  -->
+
   <div class="container main-page">
     <h1>Hello!</h1>
     <h3>I'm Jacob a professional Full Stack Web Developer.</h3>
     <button class="btn-cta">My Story</button>
   </div>
+
+  <!-- end of main  -->
 
   <div class="container about-me">
     <div class="content header">Who am I?</div>
@@ -26,6 +31,8 @@
     <div class="image-container"><img src="" alt="" /></div>
     <button class="btn-cta">Why You should hire <menu /></button>
   </div>
+
+  <!-- end of about-me  -->
 
   <div class="container gallery">
     <div class="grid-container gallery">
@@ -44,10 +51,36 @@
     </div>
   </div>
 
+  <!-- end of gallery  -->
+
   <div class="container contact-me">
     <!-- <form action=""></form> Do this form later -->
   </div>
 </main>
 
 <style>
+  nav {
+    width: 1rem;
+    height: 100vh;
+  }
+  .nav-list {
+    display: flex;
+    flex-direction: column;
+    margin-left: -0.3rem;
+    gap: 4rem;
+  }
+  .nav-element {
+    list-style: disc;
+    list-style-position: outside;
+    padding-top: 1rem;
+    font-size: 16px;
+    writing-mode: vertical-lr;
+    rotate: 180deg;
+  }
+
+  .nav-element:nth-child(4) {
+    list-style: none;
+  }
+
+  /* end of nav styling  */
 </style>
