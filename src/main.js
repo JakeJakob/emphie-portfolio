82ba2ce7ea644ec779de32b0cc79d1f9318aadae
jakeww <wlostpiotr@gmail.com>
2023-03-01 23:06:10 +0100

    Adding js form error hadnling

--- src/main.js
+++ src/main.js
@@ -3,17 +3,7 @@ import App from './App.svelte'
 
 const app = new App({
   target: document.getElementById('app'),
-}) 
-
-
-// const r = document.getElementById("image");
-// let vw = window.innerWidth;
-
-
-// for (let i = 0; i < 100; i++) {
-//   r.style.backgroundPosition = `${vw}px`;
-//   vw -= 1;
-// }
-
-
+})
 export default app
+
+
